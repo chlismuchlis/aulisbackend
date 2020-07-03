21956ec8c870275a84a1e12911f3fdcc01e3c33e
Mahmuddin Muhlis <chlismuchlis@gmail.com>
2020-07-03 21:12:59 +0700

    2

--- src/main/java/com/aulis/aulisbackend/datasource/PostgresDatasource.java
+++ src/main/java/com/aulis/aulisbackend/datasource/PostgresDatasource.java
@@ -1,4 +1,22 @@
 package com.aulis.aulisbackend.datasource;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class PostgresDatasource {
+
+    @Bean
+    @ConfigurationProperties("app.datasource") //settingan app.datasource ada pada src/main/resource/aplication.yml
+    public HikariDataSource hikariDataSource(){
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
 }
